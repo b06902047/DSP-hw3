9286c63dc7dd42d683b481fa0486dd5c7fbadf3d
陳彥 <iris8822@chenyande-MacBook-Pro.local>
2019-12-03 23:55:45 +0800

    7th

--- dsp_hw3/mydisambig.cpp
+++ dsp_hw3/mydisambig.cpp
@@ -12,8 +12,8 @@
 using namespace std;
 
 char seg_file[256];
-char mapping[256];
-char model[256];
+char ztob[256];
+char lmodel[256];
 
 int main(int argc, char *argv[]){
 	strcpy(seg_file, argv[2]);
@@ -22,17 +22,27 @@ int main(int argc, char *argv[]){
 	int order=stoi(argv[8]);
 	Vocab voc;
     Ngram lm( voc, order );
-    File lmFile( lmodel, "r" );
+    FILE lmFile( lmodel, "r" );
     lm.read(lmFile);
     lmFile.close();
-    map<string,vector<string>> dict;
+    map<char*,vector<char*>> dict;
     File *mapping=fopen(ztob,"r");
     char zbmap;
     char line[1024];
     while(!feof(mapping)){
     	fgets(line, sizeof(line), mapping);
-    	char zhu[2];
-    	char word[2];
+    	char zhu[3];
+    	char word[3];
+    	zhu[2]='\0';
+    	strncpy(zhu,line,2);
+    	vector<char*> vec;
+    	dict.insert(pair<char*, vector<char*>>(zhu, vec));
+    	for(int i=1;i<sizeof(line)/2;i++){
+    		char help[3];
+    		help[2]='\0';
+    		strncpy(help,line+3*i,2);
+    		dict[zhu].push_back(help);
+    	}
     }
 
 
