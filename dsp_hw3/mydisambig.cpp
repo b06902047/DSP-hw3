3daac255291d8b03fa8eec1bb90649ea371220b7
陳彥 <iris8822@chenyande-MacBook-Pro.local>
2019-12-05 03:07:27 +0800

    14th

--- dsp_hw3/mydisambig.cpp
+++ dsp_hw3/mydisambig.cpp
@@ -15,15 +15,16 @@ using namespace std;
 char seg_file[256];
 char ztob[256];
 char lmodel[256];
-int order;
-string line;
+int order=2;
 string problem;
-
+Vocab voc;
+Ngram lm(voc,order);
 
 map<string,vector<string>> dict;
 void Dealdiction(){
     ifstream mapping;
     mapping.open(ztob);
+    string line;
     while(getline (mapping, line)){//deal with map
         string zhu;
         zhu.assign(line.begin(),line.begin()+2);
@@ -35,7 +36,7 @@ void Dealdiction(){
     }
     mapping.close();
 }
-double probab(const char* word1,const char* word2,Ngram &lm, Vocab &voc){//P(word2|word1)
+double probab(const char* word1,const char* word2){//P(word2|word1)
     VocabIndex wid1 = voc.getIndex(word1);
     if(wid1 == Vocab_None) 
         wid1= voc.getIndex(Vocab_Unknown);
@@ -50,31 +51,30 @@ int main(int argc, char *argv[]){
 	strcpy(ztob, argv[4]);
 	strcpy(lmodel, argv[6]);
 	order=stoi(argv[8]);
-    Vocab voc;
-    Ngram lm(voc,order);
     File lmFile( lmodel, "r" );
     lm.read(lmFile);
     lmFile.close();
     Dealdiction();
     ifstream test_data;
     test_data.open(seg_file);//deal with each line
+    string line;
     while(getline(test_data,line)){//處理每一行
-        double viter[15000][15000]={{0.0}};
-        int trace[15000][15000]={{0}};
+        double viter[3000][3000]={{0.0}};
+        int trace[3000][3000]={{0}};
         auto itor = remove_if(line.begin(), line.end(), ::isspace);
         line.erase(itor, line.end());//去除空白
         string word;
         word.assign(line.begin(),line.begin()+2);
         for(int i=0;i<dict[word].size();i++)
-            viter[i][0]=probab("<s>",dict[word][i].c_str(),lm,voc);//P(第0個字｜<s>)
+            viter[i][0]=probab("<s>",dict[word][i].c_str());//P(第0個字｜<s>)
         for(int i=1;i<line.size()/2;i++){//橫軸
-            string prev(word);
+            string prev=word;
             word.assign(line.begin()+2*i,line.begin()+2*i+2);
             for(int j=0;j<dict[word].size();j++){//縱軸
                 double maxprob=-1;
                 int maxdex=-1;
                 for(int k=0;k<dict[prev].size();k++){
-                    double now=probab(dict[word][j].c_str(),dict[prev][k].c_str(),lm,voc)+viter[k][i-1];//problem
+                    double now=probab(dict[prev][k].c_str(),dict[word][j].c_str())+viter[k][i-1];//problem
                     if(now>maxprob){
                         maxprob=now;
                         maxdex=k;
@@ -84,10 +84,10 @@ int main(int argc, char *argv[]){
                 trace[j][i]=maxdex;
             }
         }
-        string prev(word);
+        string prev=word;
         double maxprob=-1;int maxdex=-1;
         for(int i=0;i<dict[prev].size();i++){
-            double now=probab(dict[prev][i].c_str(),"</s>",lm,voc)+viter[i][line.size()/2-1];
+            double now=probab(dict[prev][i].c_str(),"</s>")+viter[i][line.size()/2-1];
             if(now>maxprob){
                 maxprob=now;
                 maxdex=i;
