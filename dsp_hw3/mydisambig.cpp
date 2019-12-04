932b89fe41054c87f57db957df2b90c455d92fa3
陳彥 <iris8822@chenyande-MacBook-Pro.local>
2019-12-05 01:28:30 +0800

    14th

--- dsp_hw3/mydisambig.cpp
+++ dsp_hw3/mydisambig.cpp
@@ -62,15 +62,49 @@ int main(int argc, char *argv[]){
         double viter[15000][15000]={{0.0}};
         int trace[15000][15000]={{0}};
         auto itor = remove_if(line.begin(), line.end(), ::isspace);
-        line.erase(itor, line.end());
+        line.erase(itor, line.end());//去除空白
         string word;
         word.assign(line.begin(),line.begin()+2);
         for(int i=0;i<dict[word].size();i++)
-            viter[i][0]=probab("<s>",dict[word][i].c_str());
-        string prev(word);
-        for(int i=1;i<line.size()/2;i++){
-            
+            viter[i][0]=probab("<s>",dict[word][i].c_str());//P(第0個字｜<s>)
+        for(int i=1;i<line.size()/2;i++){//橫軸
+            string prev(word);
+            word.assign(line.begin()+2*i,line.begin()+2*i+2);
+            for(int j=0;j<dict[word].size();j++){//縱軸
+                double max=-1;
+                int maxdex=-1;
+                for(int k=0;k<dict[prev].size();k++){
+                    double now=probab(dict[word][j],dict[prev][k])+viter[k][i-1];//problem
+                    if(now>max){
+                        max=now;
+                        maxdex=k;
+                    }
+                }
+                viter[j][i]=max;
+                trace[j][i]=maxdex;
+            }
         }
+        string prev(word);
+        double max=-1;int maxdex=-1;
+        for(int i=0;i<dict[prev].size();i++){
+            double now=probab(dict[prev][i].c_str(),"</s>");
+            if(now>max){
+                max=now;
+                maxdex=k;
+            }
+        }
+        vector<string> answer;
+        int index=maxdex;
+        for(int i=line.size()/2-1;i>=0;i--){
+            string help;
+            help.assign(line.begin()+2*i,line.begin()+2*i+2);
+            answer.pushback(dict[help][index]);
+            index=trace[index][i];
+        }
+        cout << "<s> ";
+        for(int i=line.size()/2-1;i>=0;i--)
+            cout << answer[i] <<" ";
+        cout << "</s>";
     }
 	
 	return 0;
